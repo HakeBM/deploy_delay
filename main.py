5dbadaba5dc1e376deb51c2e0be3220a8acf30e3
Brayan Hernández <brayanbehc@outlook.com>
2026-01-13 11:14:28 -0600

    creando batch

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File,HTTPException, Depends
+from fastapi.response import JSONResponse
 import joblib
 import pandas as pd
-from schemas import FlightInput
+import numpy as np
+from typing import List, Dict, Any
+import io 
+import logging
+from contextlib import asynccontextmanager
+from schemas import FlightInput,PredictionFlight, BatchResponse
 
-app= FastAPI(tittle="Predictor Delay API",
+model_in_use = "predictor_delay.pkl"
+THRESHOLD =0.4
+req_colum= [
+  "airline","destination","origin","day_of_week","hour","distance_km"
+]
+app= FastAPI(tittle="Predictor Delay API", 
 description= "Predictor of delays on flights whit ML",
 version= "0.0.1"
 )
-model= joblib.load('predictor_delay.pkl')
-THRESHOLD =0.4
+model= joblib.load(model_in_use)
 @app.get("/")
 def home():
   return{"status":"API EN FUNCIONAMIENTO 🎉🎊"}
@@ -22,3 +32,5 @@ def predict_delay(flight : FlightInput):
 "delay_probability": round(float(prob),3),
 "treshold_used" : THRESHOLD
 }
+@app.post ("batch/predict")
+def predict_batch (file: UploadFile=(File...)):
