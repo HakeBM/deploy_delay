404bf4ed701515ef9909f23cbda3a36bed7501e9
Brayan Hernández <brayanbehc@outlook.com>
2026-01-13 11:37:00 -0600

    prueba1

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from schemas import FlightInput,PredictionFlight, BatchResponse
 
 model_in_use = "predictor_delay.pkl"
 THRESHOLD =0.4
-req_colum= [
+req_colum= {
   "airline","destination","origin","day_of_week","hour","distance_km"
-]
+}
 app= FastAPI(tittle="Predictor Delay API", 
 description= "Predictor of delays on flights whit ML",
 version= "0.0.1"
@@ -21,16 +21,31 @@ version= "0.0.1"
 model= joblib.load(model_in_use)
 @app.get("/")
 def home():
-  return{"status":"API EN FUNCIONAMIENTO 🎉🎊"}
+  return{"status":"API IS WORK 🎉🎊"}
+
+def predict_df(df: pd.DataFrame) -> pd.DataFrame:
+    probs = model.predict_proba(df)[:, 1]
+    df = df.copy()
+    df["delay_prediction"] = (probs >= THRESHOLD).astype(int)
+    df["delay_probability"] = probs.round(2)
+    return df
+  
 @app.post("/predict")
 def predict_delay(flight : FlightInput):
-  x= pd.DataFrame([flight.dict()])
-  prob= model.predict_proba(x)[0][1]
-  predict= int(prob>=THRESHOLD)
+  df= pd.DataFrame([flight.dict()])
+  result= predict_df(df).iloc[0]
   return{
-"delay _prediction": predict, 
-"delay_probability": round(float(prob),3),
-"treshold_used" : THRESHOLD
+"delay _prediction": int(result.delay_prediction), 
+"delay_probability": float(result.delay_probability),
 }
-@app.post ("batch/predict")
+@app.post ("/batch-predict")
 def predict_batch (file: UploadFile=(File...)):
+  if not file.filename.endswith(".csv")
+    raise HTTPException (400,"El archivo debe ser csv")
+  df=pd.read_csv(file.file)
+
+  if not req_colum,isssubset(df.columns):
+    raise HTTPException (
+      400, f"El archivo debe contener las columas: {req_colum}"
+    )
+  return predict_df(df).to_dict(orient="records")
